c78db2915ab7bb26c9c80baca10b08456e79ae85
Izmar Verhage <izmar.verhage@lunatech.nl>
2020-02-21 15:25:15 +0100

    Add js test

---
+++ js/videopoc.js
@@ -0,0 +1,12 @@
+const fps = 30;
+var previousFrameIndex = -1;
+const rectSize = 100;
+const scaleFrames = 20;
+var tiger;
+var clones = [];
+var audioSource;
+var audioCtx;
+var paths;
+const frameCount = 60;
+
+console.log("Fooooo")
